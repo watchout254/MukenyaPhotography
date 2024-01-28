b0d7a9784bf50034d0100cd332df5ccef07824ab
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-01-28 12:49:23 +0300

    wozza

--- App/views.py
+++ App/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Personal'
+    dest1.desc = 'Capture every moment'
+    dest1.price = 1500
+    #dest1.img = 'destination.jpg'
+    #dest1.offer = False
+
+    #dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dest1': dest1})
+
